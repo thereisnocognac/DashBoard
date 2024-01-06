f34452fa4988f5599a7a020d5c826fd71d5a7a76
thereisnocognac <yollovesxyra@gmail.com>
2024-01-06 04:27:24 -0500

    added author

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage= `url(${data.urls.raw})`
+        document.body.style.backgroundImage= `url(${data.urls.full})`
+        document.querySelector('#author').textContent = data.user.name
+        console.log(data)
     })
